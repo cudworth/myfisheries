7339bb028d8296ad137a781f50e0ac77942dc159
Austin Cudworth <austincudworth@gmail.com>
2021-02-01 17:33:49 -0800

    Initial Google Maps component implementation

---
+++ src/OverlayMap.js
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from 'react';
+import './OverlayMap.css';
+import { Loader } from '@googlemaps/js-api-loader';
+/* global google */
+
+function OverlayMap(props) {
+  const myRef = useRef(null);
+
+  useEffect(() => {
+    const loader = new Loader({
+      apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+      version: 'weekly',
+    });
+
+    loader.load().then(() => {
+      myRef.current = new google.maps.Map(
+        document.getElementById('overlay-map'),
+        {
+          center: { lat: -34.397, lng: 150.644 },
+          zoom: 8,
+        }
+      );
+    });
+  }, []);
+
+  return <div id="overlay-map" className="overlay-map"></div>;
+}
+
+export default OverlayMap;
